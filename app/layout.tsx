a280e645de6caafa37a30dfd839a9558eecbf8f4
license-book <licensebook@gmail.com>
2026-08-23 11:11:42 +0900

    Load refined visual theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Noto_Sans_KR } from "next/font/google";
 import "./globals.css";
+import "./theme.css";
 
 const notoSansKR = Noto_Sans_KR({
   subsets: ["latin"],

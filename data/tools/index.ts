d05998c4d94fd3f946776380d6152ce4d058feff
license-book <licensebook@gmail.com>
2026-08-26 06:07:59 +0900

    Register 20 next-wave tools

--- data/tools/index.ts
+++ data/tools/index.ts
@@ -11,6 +11,7 @@ import { workEmployeeTools } from "@/data/tools/work-employee";
 import { lifeBasicsTools } from "@/data/tools/life-basics";
 import { expansionTools } from "@/data/tools/expansion-2026-08";
 import { additionalTools } from "@/data/tools/additional-2026-08-25";
+import { nextWaveTools } from "@/data/tools/next-wave-2026-08-26";
 
 export const tools: ToolDefinition[] = [
   loanCalculatorTool, vatCalculatorTool, depositInterestTool, savingsInterestTool, compoundInterestTool, discountTool, salaryNetTool,
@@ -23,6 +24,7 @@ export const tools: ToolDefinition[] = [
   ...lifeBasicsTools,
   ...expansionTools,
   ...additionalTools,
+  ...nextWaveTools,
 ];
 
 export function getToolBySlug(slug: string) { return tools.find((tool) => tool.slug === slug); }
